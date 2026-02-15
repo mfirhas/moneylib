f85d9ee5e2c40f80d93aba1a424242ef572ffeb8
mfirhas <fathir.irhas@gmail.com>
2026-02-15 11:11:58 +0700

    Add RawMoney struct

--- src/raw_money.rs
+++ src/raw_money.rs
@@ -1,1 +1,23 @@
 //! RawMoney is money type that contains raw value of money amount without rounding to keep the precision, and choose when to round it into tender money.
+//!
+
+use crate::{Currency, Decimal};
+
+#[derive(Debug, Clone, Copy, Eq)]
+pub struct RawMoney {
+    currency: Currency,
+    amount: Decimal,
+}
+
+impl RawMoney {
+    #[inline]
+    pub const fn new(currency: Currency, amount: Decimal) -> Self {
+        Self { currency, amount }
+    }
+}
+
+impl PartialEq for RawMoney {
+    fn eq(&self, other: &Self) -> bool {
+        self.currency == other.currency && self.amount == other.amount
+    }
+}
