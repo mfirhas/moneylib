0b91c41b56b228ef6583d762d5e9515249bd25c6
mfirhas <fathir.irhas@gmail.com>
2026-05-18 16:50:01 +0700

    Remove negative ObjMoney

--- src/obj_money/ops.rs
+++ src/obj_money/ops.rs
@@ -8,25 +8,3 @@ impl ::std::ops::Neg for DynMoney {
         self.set_amount(-self.amount())
     }
 }
-
-#[inline]
-fn negate_obj(m: &dyn ObjMoney) -> Box<dyn ObjMoney> {
-    let dyn_m: DynMoney = m.try_into().unwrap_or_else(|_| unreachable!());
-    Box::new(dyn_m.set_amount(-m.amount()))
-}
-
-impl ::std::ops::Neg for Box<dyn ObjMoney> {
-    type Output = Box<dyn ObjMoney>;
-
-    fn neg(self) -> Self::Output {
-        negate_obj(self.as_ref())
-    }
-}
-
-impl ::std::ops::Neg for &dyn ObjMoney {
-    type Output = Box<dyn ObjMoney>;
-
-    fn neg(self) -> Self::Output {
-        negate_obj(self)
-    }
-}
